320507eab3da59a60921608fe3c3c5f2a2a02ec8
Alejandro Seguí <alesegdia@gmail.com>
2015-10-23 23:50:27 +0200

    moving Squad code to its own file

--- project/src/entities/Squad.lua
+++ project/src/entities/Squad.lua
@@ -1,15 +1,33 @@
 
 local Class         = require (LIBRARYPATH.."hump.class"	)
+local NOT_INVADER = 0xDEADBEEF
+
+require "src.entities.Invader"
+
 
 Squad = Class{
 
 	invaders = {},
 
-	init = function(self)
+	init = function(self, stage, slots_x, slots_y)
+		self.stage = stage
+
+		-- create empty array of invaders
+		for x = 1, slots_x do
+			self.invaders[x] = {}
+			for y = 1, slots_y do
+				self.invaders[x][y] = self:makeInvader1(x * 20, y * 10)
+			end
+		end
 	end,
 	
 	addInvader = function( self, invader )
 		table.insert( self.invaders, invader )
+	end,
+
+	makeInvader1 = function ( self, x, y )
+		local invader1_anim = newAnimation( Image.invader1, 8, 8, 0.2, 1 )
+		return Invader( self.stage, x, y, invader1_anim )
 	end,
 	
 	canMoveHorizontal = function( self, x )
--- project/src/states/Game.lua
+++ project/src/states/Game.lua
@@ -24,6 +24,8 @@ local center = {
 	y = love.graphics.getHeight() / 2
 }
 
+local scale = 4
+
 local bigFont   = love.graphics.newFont(32)
 local smallFont = love.graphics.newFont(16)
 
@@ -32,45 +34,23 @@ local smallFont = love.graphics.newFont(16)
 local world 	= bump.newWorld(8)
 local stage 	= Stage(world)
 local cam 		= camera.new(0,0,1,0)
-local squad 	= Squad()
+local squad 	= {}
 
-
-local function makeInvader1( x, y )
-	local invader1_anim = newAnimation( Image.invader1, 8, 8, 0.2, 1 )
-	return makeInvader( invader1_anim, x, y )
-end
-
-function makeInvader( anim, x, y )
-	return GameEntity( stage, x, y, anim,
-		function()
-			-- logica del invader aqui
-		end
-	)
-end
 
 function Game:enter()
 	stage:clear()
-	makeInvader1( 10, 10 )
+	squad = Squad(stage, 6, 6)
 	color = { 255, 255, 255, 0 }
-	tween(4, color, { 255, 255, 0, 255 }, 'bounce' )
-	cam:zoom(4)
+	cam:move(center.x/scale, center.y/scale)
+	cam:zoom(scale)
 end
 
 function Game:update( dt )
 	timer.update(dt)
 	stage:update(dt)
-	if gui.Button{text = "Go back"} then
-		timer.clear()
-		Gamestate.switch(Menu)
-	end
 end
 
 function Game:draw()
-	love.graphics.setFont(bigFont)
-	love.graphics.setColor(color)
-	love.graphics.rectangle("fill", 0, 0, center.x*2, color[4])
-	love.graphics.print("You lost the game.", center.x, center.y)
-	love.graphics.setFont(smallFont)
 	cam:draw( function()
 		stage:draw()
 	end )
