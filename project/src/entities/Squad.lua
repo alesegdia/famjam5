06b2b14a472634bbcecab94db7649bc7d914274d
Alejandro Seguí <alesegdia@gmail.com>
2015-10-24 02:01:57 +0200

    working on squad movement

--- project/src/entities/Squad.lua
+++ project/src/entities/Squad.lua
@@ -1,5 +1,7 @@
 
 local Class         = require (LIBRARYPATH.."hump.class"	)
+local vector 		= require (LIBRARYPATH.."hump.vector")
+local constants 	= require ("constants")
 local NOT_INVADER = 0xDEADBEEF
 
 require "src.entities.Invader"
@@ -9,14 +11,46 @@ Squad = Class{
 
 	invaders = {},
 
+	nextDown = constants.SQUAD_TIME_TO_DOWN,
+
 	init = function(self, stage, slots_x, slots_y)
 		self.stage = stage
+		self.position = vector.new(0,0)
 
 		-- create empty array of invaders
 		for x = 1, slots_x do
 			self.invaders[x] = {}
 			for y = 1, slots_y do
 				self.invaders[x][y] = self:makeInvader1(x * 20, y * 10)
+				self.invaders[x][y].aabb.isInvader = true
+			end
+		end
+	end,
+
+	step = function(self, dt)
+		
+		self.nextDown = self.nextDown - dt
+		if self.nextDown <= 0 then
+			self.nextDown = constants.SQUAD_TIME_TO_DOWN
+			self:moveBlockDown()
+		end
+
+
+		if love.keyboard.isDown("a") then
+			self:moveHorizontal(-1)
+		elseif love.keyboard.isDown("d") then
+			self:moveHorizontal(1)
+		end
+	end,
+
+	moveBlockDown = function(self)
+		local filter = function(item, other)
+			print(item.isInvader)
+		end
+		for _,invader_row in ipairs(self.invaders) do
+			for _,invader in ipairs(invader_row) do
+				local x, y = self.stage.world:getRect(invader.aabb)
+				self.stage.world:move(invader.aabb, x, y + constants.SQUAD_DOWN_DISTANCE, filter)
 			end
 		end
 	end,
@@ -25,15 +59,55 @@ Squad = Class{
 		table.insert( self.invaders, invader )
 	end,
 
+	move = function(self, direction, quantity)
+		assert( math.abs(direction) == 1, "Direction must be 1 or -1" )
+		local movement = direction * quantity
+	end,
+
 	makeInvader1 = function ( self, x, y )
 		local invader1_anim = newAnimation( Image.invader1, 8, 8, 0.2, 1 )
 		return Invader( self.stage, x, y, invader1_anim )
 	end,
-	
-	canMoveHorizontal = function( self, x )
-		for _,invader in self.invaders do
-			-- local aX, aY, cols, len = world:check(
+
+	moveHorizontal = function( self, dx )
+		local filter = function(item, other)
+			if 		other.isInvader 	then return "cross"
+			elseif 	other.isBleh 		then return "touch"
+			else return nil
+			end
 		end
+		if self:canMoveHorizontal( dx ) then
+			for _,invader_row in ipairs(self.invaders) do
+				for _,invader in ipairs(invader_row) do
+					local x, y = self.stage.world:getRect(invader.aabb)
+					self.stage.world:move(invader.aabb, x + dx, y)
+				end
+			end
+		end
+	end,
+	
+	canMoveHorizontal = function( self, dx )
+		local filter = function(item, other)
+			if 		other.isInvader 	then return "cross"
+			elseif 	other.isBleh 		then return "touch"
+			else return nil
+			end
+		end
+		local canMove = true
+		print(self.invaders)
+		for _,invader_row in ipairs(self.invaders) do
+			for _,invader in ipairs(invader_row) do
+				--[[
+				local x, y = self.stage.world:getRect(invader.aabb)
+				local aX, aY, cols, len = world:check(self.aabb, x + dx, y)
+				if len ~= 0 then
+					canMove = false
+					break
+				end
+				]]--
+			end
+		end
+		return canMove
 	end
 	
 }
