9997c4c09c44c35182ae630fff1084c1381f9ad2
Alejandro Seguí <alesegdia@gmail.com>
2015-10-24 16:25:49 +0200

    fixing shield anim selection bug; shield bigger

--- project/src/entities/Shield.lua
+++ project/src/entities/Shield.lua
@@ -6,24 +6,25 @@ local constants 	= require ("constants")
 require "src.entities.GameEntity"
 
 local shield_anims = {}
-shield_anims[1] = newAnimation( Image.defense, 10, 5, 0.2, 1 )
-shield_anims[2] = newAnimation( Image.defense, 10, 5, 0.2, 1 )
-shield_anims[3] = newAnimation( Image.defense, 10, 5, 0.2, 1 )
-shield_anims[4] = newAnimation( Image.defense, 10, 5, 0.2, 1 )
+shield_anims[1] = newAnimation( Image.defense, 24, 12, 0.2, 1 )
+shield_anims[2] = newAnimation( Image.defense, 24, 12, 0.2, 1 )
+shield_anims[3] = newAnimation( Image.defense, 24, 12, 0.2, 1 )
+shield_anims[4] = newAnimation( Image.defense, 24, 12, 0.2, 1 )
 
 Shield = Class {
 	
 	health = constants.SHIELD_HEALTH,
 
 	init = function(self, stage, x, y)
-		self = GameEntity(self, stage, x, y, shield_anim_1, { isShield = true }, self.step )
+		self = GameEntity.init(self, stage, x, y, shield_anims[1], { isShield = true }, self.step )
 	end,
 
 	step = function(self, dt)
 		if self.health <= 0 then self.dead = true end
 		local selected_anim_index = math.ceil(4 * self.health / constants.SHIELD_HEALTH)
+		if selected_anim_index == 0 then selected_anim_index = 1 end
 		assert( selected_anim_index > 0 and selected_anim_index <= 4, "Invalid shield anim index" )
-		self.anim = shields_anim[selected_anim_index]
+		self.anim = shield_anims[selected_anim_index]
 	end
 }
 
